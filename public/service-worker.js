8d20fb5a32dff8ff97508d62afe003c149c513b2
crricardorendon-droid <cr.ricardorendon@gmail.com>
2025-08-25 14:03:45 -0300

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('iciv-cache-v1').then((cache) => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.webmanifest',
+    ]))
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
---
+++ src/AppIngenieriaCivil.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function AppIngenieriaCivil(){return <h1>App Ingeniería Civil</h1>}
