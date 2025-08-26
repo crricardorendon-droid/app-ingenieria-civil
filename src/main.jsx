5f9c5ef52a94759b7bdb2a1fbb30a7191a2955f0
crricardorendon-droid <cr.ricardorendon@gmail.com>
2025-08-26 10:14:04 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,4 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './AppIngenieriaCivil.jsx'
 createRoot(document.getElementById('root')).render(<App />)
+import './mobile.css';
